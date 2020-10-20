3d1e4c4692f6472d3d8e9a37e09bb2b895416819
sebalopez <seby.lopez@gmail.com>
2020-10-20 18:29:29 -0300

    updated api vars

--- session 1/launch.sh
+++ session 1/launch.sh
@@ -11,14 +11,16 @@ docker run -d \
      postgres
 # launch API server
 docker run -d \
-   --name base-api \
-   -e DATABASE_URL=postgres://apibase:mysecretpassword@base-db/apibase \
-   -e SERVER_URL=base-api:3000 \
-   -e PASSWORD_RESET_URL=http://base-api:3000 \
-   -p 3000:3000 \
-   --network local \
+    --name base-api \
+    -e DATABASE_URL=postgres://apibase:mysecretpassword@base-db/apibase \
+    -e SERVER_HOST=base-api \
+    -e PASSWORD_RESET_URL=http://base-api:3000 \
+    -p 3000:3000 \
+    --network local \
     rootstrap/rails-base
 # Run migrations 
 docker exec base-api rails db:create db:migrate 
+# compile assets
+docker exec base-api rails assets:precompile 
 # Create test admin user
 docker exec base-api rails db:seed
