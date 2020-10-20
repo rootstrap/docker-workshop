21114a848f672030980f04c64ed77791cde1ee98
sebalopez <seby.lopez@gmail.com>
2020-10-20 18:29:36 -0300

    session 2

--- session 2/launch.sh
+++ session 2/launch.sh
@@ -1,6 +1,25 @@
-# launch FE
+### RUNTIME COMPILATION
+# Build
+docker build -t rootstrap/react-base .
+# Launch
 docker run -d \
     --name base-fe \
     --network local \
-    -p 8080:3000 \
+    -e API_URL=http://localhost:3000/api/v1 \
+    -e PORT=8080 \
+    -p 8080:8080 \
+    rootstrap/react-base
+
+
+### BUILDTIME COMPILATION
+# Build
+docker build \
+    --build-arg API_URL=http://localhost:3000/api/v1 \
+    --build-arg PORT=8080 \
+    -t rootstrap/react-base .
+# Launch
+docker run -d \
+    --name base-fe \
+    --network local \
+    -p 8080:8080 \
     rootstrap/react-base
