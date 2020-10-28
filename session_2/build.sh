f5ac9c25ff881829032288df9be408a632f98bbc
sebalopez <seby.lopez@gmail.com>
2020-10-28 09:08:18 -0300

    updated session 2 files

--- session_2/build.sh
+++ session_2/build.sh
@@ -1,4 +1,9 @@
+# Clone repo and move Dockerfile with buildtime compilation + dockerignore
 git clone git@github.com:rootstrap/react-base.git
-cp Dockerfile2 react-base/Dockerfile
+cp Dockerfile_2 react-base/Dockerfile
 cp .dockerignore react-base/
-cd react-base && docker build -t rootstrap/react-base .
+# Build using environment vars as build arguments
+cd react-base && docker build \
+    --build-arg API_URL=http://localhost:3000/api/v1 \
+    --build-arg PORT=8080 \
+    -t rootstrap/react-base .
--- session_2/launch.sh
+++ session_2/launch.sh
@@ -1,23 +1,6 @@
-### RUNTIME COMPILATION
-# Build
-docker build -t rootstrap/react-base .
-# Launch
-docker run -d \
-    --name base-fe \
-    --network local \
-    -e API_URL=http://localhost:3000/api/v1 \
-    -e PORT=8080 \
-    -p 8080:8080 \
-    rootstrap/react-base
-
-
-### BUILDTIME COMPILATION
-# Build
-docker build \
-    --build-arg API_URL=http://localhost:3000/api/v1 \
-    --build-arg PORT=8080 \
-    -t rootstrap/react-base .
-# Launch
+# Launch DB and BE
+../session_1/launch.sh
+# Launch FE
 docker run -d \
     --name base-fe \
     --network local \
