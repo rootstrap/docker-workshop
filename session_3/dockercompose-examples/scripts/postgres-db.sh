3cdcb7fcfaf2f1bdfec222e9ac4baf1b842fce66
sebalopez <seby.lopez@gmail.com>
2020-10-30 16:47:38 -0300

    fixed compose file and removed unneeded files

--- session_3/dockercompose-examples/scripts/postgres-db.sh
+++
@@ -1,8 +0,0 @@
-docker run --rm \
-  --name base-db \
-    -e POSTGRES_USER=apibase \
-    -e POSTGRES_PASSWORD=password0 \
-    -v /tmp/data:/var/lib/postgresql/data \
-    -p 5432:5432 \
-    --network backend \
-     postgres
